2a2275b6c505611f20625c07b670ad08045e8e6b
prabina dhungana <dhungapbna@gmail.com>
2026-09-02 09:32:33 +0545

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,19 @@
 function About() {
   return (
-    <section>
+    <section id="about" className="about">
       <h2>About Me</h2>
-      <p>Computer Engineer | AI & Machine Learning Enthusiast | Developer | React & Django | Building intelligent solutions with code... </p>
+
+      <p>
+        I’m a Computer Engineer passionate about Artificial Intelligence,
+        Machine Learning, and modern web development. I enjoy building
+        intelligent and user-friendly applications using technologies like
+        React and Django.
+      </p>
+
+      <p>
+        I’m constantly learning new technologies and turning ideas into
+        practical solutions through code.
+      </p>
     </section>
   );
 }
